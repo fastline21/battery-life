9c369e083fe2ddb948df7e0623e045a1274c9287
Joshwa Facistol <fastline2123@gmail.com>
2021-07-10 00:56:41 +0800

    enhance battery notification

--- main.py
+++ main.py
@@ -1,4 +1,43 @@
 import psutil
+from win10toast import ToastNotifier
+import chime
+import time
 
-battery = psutil.sensors_battery()
-print("Battery percentage : ", battery.percent)
+chime.theme("material")
+show_notification = False
+
+
+def batteryNotification(message):
+    toaster = ToastNotifier()
+    toaster.show_toast("Battery Life", message, duration=10)
+
+
+def checkBatteryPercent(battery):
+    if battery.percent == 100:
+        batteryNotification("Full Battery")
+    elif battery.percent == 10:
+        batteryNotification("Low Battery")
+    elif battery.percent == 5:
+        batteryNotification("Critical Battery")
+
+
+def getBatteryLife():
+    global show_notification
+    battery = psutil.sensors_battery()
+
+    if battery.power_plugged:
+        if show_notification == False:
+            batteryNotification("Charging Laptop")
+            show_notification = True
+    else:
+        show_notification = False
+
+    checkBatteryPercent(battery)
+
+
+chime.success()
+print("Battery Life starting...")
+
+while True:
+    getBatteryLife()
+    time.sleep(10)
